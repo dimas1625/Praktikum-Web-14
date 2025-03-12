d2ca4445e75bb9a4ee6b69f027227511ff49afa2
Dimas Nugroho <dimasceper358@gmail.com>
2025-03-12 20:02:22 +0700

    js4-praktikum2.6/1-2

--- Minggu 4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu 4/PWL_POS/app/Http/Controllers/UserController.php
@@ -9,22 +9,8 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $user = UserModel::create([
-            'username' => 'manager11',
-            'nama'     => 'Manager11',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
-        ]);
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
 
-        $user->username = 'manager12';
-        $user->save();
-
-        // Memeriksa perubahan
-        $user->wasChanged(); // true
-        $user->wasChanged('username'); // true
-        $user->wasChanged(['username', 'level_id']); // true
-        $user->wasChanged('nama'); // false
-        $user->wasChanged(['nama', 'username']); // true
-        dd($user->wasChanged(['nama', 'username'])); // true
     }
 }
