11623547450678ecf7f68a5a907ac53fe422c272
Dimas Nugroho <dimasceper358@gmail.com>
2025-03-12 19:30:45 +0700

    js4-praktikum2.5-1

--- Minggu 4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu 4/PWL_POS/app/Http/Controllers/UserController.php
@@ -10,18 +10,30 @@ class UserController extends Controller
 {
     public function index(){
 
-        $manager = UserModel::firstOrNew(
-            [
-            'username' => 'manager33',
-            'nama'     => 'Manager Tiga Tiga',
+        $user = UserModel :: create([
+            'username' => 'manager55',
+            'nama' => 'Manager55',
             'password' => Hash::make('12345'),
-            'level_id' => 2
-            ]
-        );
-        
-        $user->save();
-
-        // Kirim data ke view
-        return view('user', ['data' => $manager]);
+            'level_id' => 2,
+            
+            ]);
+            
+            $user->username = 'manager56';
+            
+            $user->isDirty(); // true
+            $user->isDirty('username'); // true
+            $user->isDirty('nama'); // false
+            $user->isDirty(['nama', 'username']); // true
+            
+            $user->isClean(); // false
+            $user->isClean('username'); // false
+            $user->isClean('nama'); // true
+            $user->isClean(['nama', 'username']); // false
+            
+            $user->save();
+            
+            $user->isDirty();
+            $user->isClean();
+            dd($user->isDirty());
     }   
 }
