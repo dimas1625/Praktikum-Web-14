3e42dcc8c8d15818502803b6ffc9fb7509091281
Dimas Nugroho <dimasceper358@gmail.com>
2025-03-13 14:28:34 +0700

    js4-praktikum2.7_2

--- Minggu 4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu 4/PWL_POS/app/Http/Controllers/UserController.php
@@ -9,49 +9,7 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
-    }
-
-    // Tambahkan method tambah untuk menampilkan form tambah user
-    public function tambah(){
-        return view('user_tambah');
-    }
-    // Method untuk menyimpan data user dari form tambah
-    public function tambah_simpan(Request $request) //menerima form melalui inputan
-    {
-        UserModel::create([ //membuat data baru
-            'username' => $request->username,
-            'nama' => $request->nama,
-            'password' => Hash::make('$request->password'),
-            'level_id' => $request->level_id
-        ]);
-        return redirect('/user');   //mengirimkan tampilan kepada view 'user.blade.php'
-    }
-    public function ubah($id)   //mengambil data yang sesuai dengan ID yang dipilih
-    {
-        $user = UserModel::find($id);   //mencari ID yang dipilih
-        return view('user_ubah', ['data' => $user]);    //mengirimkan tampilan ke 'user.blade.php'
-    }
-    public function ubah_simpan($id, Request $request){
-
-        $user = UserModel :: find($id);
-
-        $user->username = $request->username;
-        $user->nama = $request->nama;
-        $user->password = Hash::make('$request->password');
-        $user->level_id = $request->level_id;
-
-        $user->save();
-
-        return redirect('/user');
-    }
-    public function hapus($id) {
-
-        $user = UserModel::find($id);
-        $user->delete();
-
-        return redirect('/user');
-        
+        $user = UserModel::with('level')->get();
+        dd($user);
     }
 }
