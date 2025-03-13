05b1771d030f1ac07e44c0305c4418a1d18f4a5d
Dimas Nugroho <dimasceper358@gmail.com>
2025-03-13 14:41:39 +0700

    js4-praktikum2.7_4-5

--- Minggu 4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu 4/PWL_POS/app/Http/Controllers/UserController.php
@@ -10,6 +10,6 @@ class UserController extends Controller
 {
     public function index(){
         $user = UserModel::with('level')->get();
-        dd($user);
+        return view('user', ['data' => $user]);
     }
 }
--- Minggu 4/PWL_POS/resources/views/user.blade.php
+++ Minggu 4/PWL_POS/resources/views/user.blade.php
@@ -4,22 +4,31 @@
     <title>Data User</title>
 </head>
 <body>
-    <h1>Form Tambah Data User</h1>
-    <form method="post" action="/user/tambah_simpan"></form>
-        {{ csrf_field() }}
-
-        <label>Username</label>
-        <input type="text" name="username" placeholder="Masukkan Username">
-        <br>
-        <label>Nama</label>
-        <input type="text" name="nama" placeholder="Masukkan Nama">
-        <br>
-        <label>Password</label>
-        <input type="password" name="password" placeholder="Masukkan Password">
-        <br>
-        <label>Level ID</label>
-        <input type="number" name="level_id" placeholder="Masukkan ID Level">
-        <br><br>
-        <input type="submit" class="btn btn-success" value="Simpan">
+    <h1>Data User</h1>
+     <a href="/user/tambah">+ Tambah User</a>
+     <table border="1" cellpadding="2" cellspacing="0">
+         <tr>
+             <td>ID</td>
+             <td>Username</td>
+             <td>Nama</td>
+             <td>ID Level Pengguna</td>
+             <td>Kode Level</td>
+             <td>Nama Level</td>
+             <td>Aksi</td>
+         </tr>
+         @foreach ($data as $d)
+         <tr>
+             <td>{{ $d->user_id }}</td>
+             <td>{{ $d->username }}</td>
+             <td>{{ $d->nama }}</td>
+             <td>{{ $d->level_id }}</td>
+             <td>{{ $d->level->level_kode }}</td>
+             <td>{{ $d->level->level_nama }}</td>
+             <td>
+                 <a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">Hapus</a>
+             </td>
+         </tr>
+         @endforeach
+     </table>
 </body>
 </html>
