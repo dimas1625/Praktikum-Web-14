6fc17fad3c6d1eee86b69dfd5b71e980fc412cf3
Dimas Nugroho <dimasceper358@gmail.com>
2025-03-18 12:01:12 +0700

    js5-praktikum2

--- Minggu 5/PWL_POS/resources/js/app.js
+++ Minggu 5/PWL_POS/resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+import "../sass/app.scss";
+import 'laravel-datatables-vite';
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+    },
+});
--- Minggu 5/PWL_POS/resources/views/layout/app.blade.php
+++ Minggu 5/PWL_POS/resources/views/layout/app.blade.php
@@ -3,15 +3,18 @@
 {{-- Extend and customize the browser title --}}
 @section('title')
     {{ config('adminlte.title') }}
-    @hasSection('subtitle') | @yield('subtitle') @endif
+    @hasSection('subtitle')
+        | @yield('subtitle')
+    @endif
 @stop
+
+@vite('resources/js/app.js')
 
 {{-- Extend and customize the page content header --}}
 @section('content_header')
     @hasSection('content_header_title')
         <h1 class="text-muted">
             @yield('content_header_title')
-
             @hasSection('content_header_subtitle')
                 <small class="text-dark">
                     <i class="fas fa-xs fa-angle-right text-muted"></i>
@@ -37,28 +40,30 @@
             {{ config('app.company_name', 'My company') }}
         </a>
     </strong>
-@stop
-
-{{-- Add common JavaScript/jQuery code --}}
-@push('js')
-    <script>
-        $(document).ready(function() {
-            // Add your common script logic here...
-        });
-    </script>
-@endpush
-
-{{-- Add common CSS customizations --}}
-@push('css')
-    <style type="text/css">
-        /* You can add AdminLTE customizations here */
-        /*
-        .card-header {
-            border-bottom: none;
-        }
-        .card-title {
-            font-weight: 600;
-        }
-        */
-    </style>
-@endpush
+    
+    @stop
+    
+    {{-- Add common Javascript/Jquery code --}}
+    @push('js')
+        <script src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script>
+    @endpush
+    
+    @stack('scripts')
+    
+    {{-- Add common CSS customizations --}}
+    @push('css')
+        <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.css" />
+    
+        <style type="text/css">
+            {{-- You can add AdminLTE customizations here --}}
+            /*
+            .card-header {
+                border-bottom: none;
+            }
+            .card-title {
+                font-weight: 600;
+            }
+            */
+        </style>
+    @endpush
+    
