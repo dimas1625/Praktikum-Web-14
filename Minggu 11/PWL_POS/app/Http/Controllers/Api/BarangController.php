8484960815bd4433b48bd7b74024d40759eb828e
Dimas nugroho <144247337+dimas1625@users.noreply.github.com>
2025-05-21 22:18:01 +0700

    js 11 Tugas praktikum

--- Minggu 11/PWL_POS/app/Http/Controllers/Api/BarangController.php
+++ Minggu 11/PWL_POS/app/Http/Controllers/Api/BarangController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use App\Models\BarangModel;
 use Illuminate\Http\Request;
 
@@ -12,17 +13,41 @@ class BarangController extends Controller
     { 
         return BarangModel::all(); 
     }
-    public function store(Request $r) {
-        $brg = BarangModel::create($r->all());
-        return response()->json($brg, 201);
+    public function store(Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'kategori_id'  => ['required', 'integer'],
+            'barang_kode'  => ['required', 'string', 'min:3', 'max:10', 'unique:m_barang,barang_kode'],
+            'barang_nama'  => ['required', 'string', 'min:3', 'max:100'],
+            'harga_beli'   => ['required', 'numeric', 'min:0'],
+            'harga_jual'   => ['required', 'numeric', 'min:0'],
+            'image' => ['required', 'image', 'mimes:jpeg,png,jpg,gif', 'max:2048']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $image = $request->file('image');
+
+        $barang = BarangModel::create([
+            'kategori_id' => $request->kategori_id,
+            'barang_kode' => $request->barang_kode,
+            'barang_nama' => $request->barang_nama,
+            'barang_stok' => 0,
+            'harga_beli' => $request->harga_beli,
+            'harga_jual' => $request->harga_jual,
+            'image' => $image->hashName(),
+        ]);
+        return response()->json($request, 201);
     }
 
     public function show(BarangModel $barang) { 
         return $barang; 
     }
 
-    public function update(Request $r, BarangModel $barang) {
-        $barang->update($r->all());
+    public function update(Request $request, BarangModel $barang) {
+        $barang->update($request->all());
         return response()->json($barang);
     }
     
